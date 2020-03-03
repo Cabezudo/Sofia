beeac9f4cd0b202d7fb408c7499eeb72b87c1901
Esteban Cabezudo <esteban@cabezudo.net>
2020-03-02 23:57:29 -0600

    Sites menu

---
+++ src/net/cabezudo/sofia/core/sites/SiteNamesValidationService.java
@@ -0,0 +1,49 @@
+package net.cabezudo.sofia.core.sites;
+
+import java.sql.SQLException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import net.cabezudo.sofia.core.system.SystemMonitor;
+import net.cabezudo.sofia.core.users.User;
+import net.cabezudo.sofia.core.ws.parser.tokens.Token;
+import net.cabezudo.sofia.core.ws.parser.tokens.Tokens;
+import net.cabezudo.sofia.core.ws.responses.Response;
+import net.cabezudo.sofia.core.ws.responses.ValidationResponse;
+import net.cabezudo.sofia.core.ws.servlet.services.ValidationService;
+
+/**
+ * @author <a href="http://cabezudo.net">Esteban Cabezudo</a>
+ * @version 0.01.00, 2019.10.09
+ */
+public class SiteNamesValidationService extends ValidationService {
+
+  private final Tokens tokens;
+
+  public SiteNamesValidationService(HttpServletRequest request, HttpServletResponse response, Tokens tokens) throws ServletException {
+    super(request, response);
+    this.tokens = tokens;
+  }
+
+  @Override
+  public void execute() throws ServletException {
+    int siteId;
+    String name;
+    Token token;
+
+    User owner = super.getUser();
+
+    name = tokens.getValue("name").toString();
+
+    try {
+      Site siteToValidate = SiteManager.getInstance().getByName(name);
+      if (siteToValidate != null) {
+        sendResponse(new ValidationResponse(Response.Status.ERROR, "site.name.exist"));
+      }
+      sendResponse(new ValidationResponse(Response.Status.OK, "site.name.ok"));
+    } catch (SQLException e) {
+      SystemMonitor.log(e);
+      sendError(HttpServletResponse.SC_SERVICE_UNAVAILABLE, "Service unavailable");
+    }
+  }
+}
