55f52d1bd68409f35acf101dbad0226e6bb8785f
Esteban Cabezudo <esteban@cabezudo.net>
2020-09-23 21:16:14 -0500

    Category houres in html

--- src/main/java/net/cabezudo/sofia/food/CategoryHoursList.java
+++ src/main/java/net/cabezudo/sofia/food/CategoryHoursList.java
@@ -3,7 +3,6 @@ package net.cabezudo.sofia.food;
 import java.util.Map;
 import java.util.Map.Entry;
 import net.cabezudo.json.JSONPair;
-import net.cabezudo.json.values.JSONArray;
 import net.cabezudo.json.values.JSONObject;
 import net.cabezudo.sofia.core.schedule.BusinessHours;
 import net.cabezudo.sofia.restaurants.Restaurant;
@@ -22,18 +21,12 @@ public class CategoryHoursList {
     hoursByCategoryMap = businessHours.getHoursById(categories);
   }
 
-  public JSONArray toJSONTree() {
-    JSONArray jsonArray = new JSONArray();
+  public JSONObject toJSONTree() {
+    JSONObject jsonObject = new JSONObject();
     for (Entry<Category, CategoryHours> entry : hoursByCategoryMap.entrySet()) {
-      JSONObject jsonCategoryHours = new JSONObject();
       Category category = entry.getKey();
-      JSONObject jsonCategory = new JSONObject();
-      jsonCategory.add(new JSONPair("id", category.getId()));
-      jsonCategory.add(new JSONPair("name", category.getName()));
-      jsonCategoryHours.add(new JSONPair("category", jsonCategory));
-      jsonCategoryHours.add(new JSONPair("hours", entry.getValue().toJSONTree()));
-      jsonArray.add(jsonCategoryHours);
+      jsonObject.add(new JSONPair(category.getName(), entry.getValue().toJSONTree()));
     }
-    return jsonArray;
+    return jsonObject;
   }
 }
