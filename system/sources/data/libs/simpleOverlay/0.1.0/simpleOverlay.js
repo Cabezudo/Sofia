450c83b019789438fea6fbaa32245cc9b6bb4de7
Esteban Cabezudo <esteban@cabezudo.net>
2020-09-25 01:07:08 -0500

    Simple overflow

---
+++ system/sources/data/libs/simpleOverlay/0.1.0/simpleOverlay.js
@@ -0,0 +1,46 @@
+/*
+ * Created on: 30/10/2018
+ * Author:     Esteban Cabezudo
+ */
+
+/* global Core */
+
+const simpleOverlay = ({ id = null, scrollElementId = null, scrollElement = null } = {}) => {
+  let
+          overlay,
+          lastOverflowValue;
+
+  const validateOptions = () => {
+    if (id !== null && id.length === 0) {
+      throw Error(`Invalid element id: ${id}`);
+    }
+    if (scrollElementId !== null) {
+      scrollElement = Core.validateById(scrollElementId);
+    }
+  };
+  const createGUI = () => {
+    overlay = document.createElement('DIV');
+    overlay.className = 'simpleOverlay';
+    document.body.appendChild(overlay);
+  };
+  const assignTriggers = () => {
+    overlay.addEventListener('click', () => {
+      overlay.style.display = 'none';
+      if (scrollElement) {
+        scrollElement.style.overflow = lastOverflowValue;
+      }
+    });
+  };
+  this.show = () => {
+    overlay.style.display = 'block';
+    if (scrollElement) {
+      lastOverflowValue = scrollElement.style.overflow;
+      scrollElement.style.overflow = 'hidden';
+    }
+  };
+  validateOptions();
+  createGUI();
+  assignTriggers();
+  return this;
+}
+;
