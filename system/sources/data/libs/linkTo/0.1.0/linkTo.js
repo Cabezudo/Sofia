c2860491469b63349f203758c7444b62df74df5b
Esteban Cabezudo <esteban@cabezudo.net>
2020-10-01 12:15:04 -0500

    Add order button

--- system/sources/data/libs/linkTo/0.1.0/linkTo.js
+++ system/sources/data/libs/linkTo/0.1.0/linkTo.js
@@ -8,21 +8,22 @@
 const linkTo = ({ onClick = null, id = null, element = null } = {}) => {
   const validateOptions = () => {
     if (id !== null) {
-      element = Core.validateById(id);
+      this.element = Core.validateById(id);
     } else {
       if (element === null) {
         throw Error('You must define a property id or a property element.');
       }
+      this.element = element;
     }
     if (onClick === null) {
       throw Error('You must define a url or function in a property onClick.');
     }
   };
   const createGUI = () => {
-    element.classList.add('linkTo');
-    const html = element.innerHTML;
-    element.innerHTML = `<div>${html}</div>`;
-    element.onclick = () => {
+    this.element.classList.add('linkTo');
+    const html = this.element.innerHTML;
+    this.element.innerHTML = `<div>${html}</div>`;
+    this.element.onclick = () => {
       if (typeof onClick === 'function') {
         onClick(this);
       } else {
@@ -35,5 +36,6 @@ const linkTo = ({ onClick = null, id = null, element = null } = {}) => {
   validateOptions();
   createGUI();
   assignTriggers();
+  return this;
 }
 ;
